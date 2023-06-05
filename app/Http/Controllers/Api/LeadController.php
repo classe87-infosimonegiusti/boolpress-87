a17ff7e803922b6bb1516ba8ce830196e1eafd95
Simone Giusti <info@simonegiusti.it>
2023-06-05 13:00:05 +0200

    Lc concluso

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    public function store(Request $request) {
+
+        $data = $request->all();
+
+        $validator = Validator::make($data,
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'message' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ]
+            );
+        }
+
+        $pippo = new Lead();
+        $pippo->fill($data);
+        $pippo->save();
+
+        $oggettoNewContact = new NewContact($pippo);
+
+        Mail::to('info@simonegiusti.it')->send($oggettoNewContact);
+
+        return response()->json(
+            [
+                'success' => true
+            ]
+        );
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/{slug}', [PostController::class, 'show']);
+
+Route::post('/contacts', [LeadController::class, 'store']);
